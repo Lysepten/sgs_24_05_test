4594986adfc3432b65cc494cbbd22e6863f1a51e
lysepten <tlstlsrbrb11@gmail.com>
2024-05-17 21:31:05 +0900

    jsp 매핑

--- src/main/java/com/example/demo/controller/tourismController.java
+++ src/main/java/com/example/demo/controller/tourismController.java
@@ -1,10 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class tourismController {
-	
-	public static void main(String[] args) {
-		
-		
-		
+
+	@RequestMapping("/usr/home/main")
+	public String test1() {
+
+		return "/usr/home/main";
+	}
+
+	@RequestMapping("/")
+	public String test2() {
+
+		return "/usr/home/main";
 	}
 }
